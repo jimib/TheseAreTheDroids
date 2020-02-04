ca48c6410eff85881799fe60edc0b40123423983
jimib <jimi.bailey@gmail.com>
2020-02-04 21:17:09 +0000

    Motor code updated to allow for testing as USB device or over Radio

--- Microbit/projects/Motors/main.ts
+++ Microbit/projects/Motors/main.ts
@@ -1,40 +1,80 @@
 let speed = 0;
+let count = 0;
+
+let name = control.deviceSerialNumber().toString();
+
+let delimiter = serial.delimiters(Delimiters.Comma);
+serial.onDataReceived(delimiter, () => {
+    count++;
+    printMessage(count.toString());
+    parseMessage( serial.readUntil( delimiter ) );
+});
+
+function printMessage( message:string ){
+    serial.writeLine( message );
+}
+
+function parseMessage( message:string ){
+    let parts = message.split(":");
+    switch( parts[0] ){
+        case name:
+        case "":
+            //action this
+            printMessage("Action:" + parts[1] );
+            switch( parts[1] ){
+                case "register":
+                    printMessage( name );
+                break;
+                case "setSpeed":
+                    let s = parseInt( parts[2] );
+                    printMessage( "setSpeed:" + s );
+                    setSpeed( s );
+                break;
+            }
+        break;
+    }
+}
 
 radio.setGroup(1);
 
 radio.onDataPacketReceived((packet) => {
     basic.clearScreen();
-    speed = packet.receivedNumber;
-    updateSpeed();
+    basic.showString("D");
+    //parse the message
+    parseMessage( packet.receivedString );
+    // speed = packet.receivedNumber;
+    // updateSpeed();
 });
 
 
 input.onButtonPressed(Button.A, function () {
-    speed = Math.max( -1023, speed - 100 );
-    updateSpeed();
+    setSpeed( speed - 100 );
 });
 
 input.onButtonPressed(Button.B, function () {
-    speed = Math.min(1023, speed + 100 );
-    updateSpeed();
+    setSpeed( speed + 100 );
 });
 
 pins.analogWritePin(AnalogPin.P0, speed);
 pins.analogSetPeriod(AnalogPin.P0, 20000);
-updateSpeed();
-basic.showString("M");
 
-function updateSpeed() {
-    setValue(speed);
-    //determine the direction
-    pins.digitalWritePin(DigitalPin.P1, speed > 0 ? 0 : 1 );
-    pins.digitalWritePin(DigitalPin.P2, speed > 0 ? 1 : 0 );
-    //determine the magnitude
-    pins.analogWritePin(AnalogPin.P0, Math.abs(speed) );
+setSpeed( 0 );
+
+function setSpeed( val:number ) {
+    if( !isNaN( val ) ){
+        //set a record of the value
+        speed = Math.min( 1023, Math.max( -1023, val ) );
+        //determine the direction
+        pins.digitalWritePin(DigitalPin.P1, speed > 0 ? 0 : 1 );
+        pins.digitalWritePin(DigitalPin.P2, speed > 0 ? 1 : 0 );
+        //determine the magnitude
+        pins.analogWritePin(AnalogPin.P0, Math.abs(speed) );
+        //update the led to help us debug
+        writeSpeedToLed(speed);
+    }
 }
 
-function setValue(value: number) {
-    //led.toggle(0, 0);
+function writeSpeedToLed(value: number) {
     //return;
     for (let x = 0; x < 5; x++) {
         for (let y = 0; y < 5; y++) {
